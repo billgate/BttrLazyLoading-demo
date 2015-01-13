d97a9bc77e48c6a6d15a77937e441d1b6f34b524
Julien Renaux <julien.renaux@livingobjects.com>
2015-01-13 19:17:59 +0100

    adding multiserve animation demo

---
+++ demo/multiserve-animations.php
@@ -0,0 +1,157 @@
+<?php
+define('DEMO', 'multiserve-animations');
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Demo Multiserve Animations | BttrLazyLoading</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="BttrLazyLoading is a Jquery plugin that allows your web application to only load images within the viewport. It also allows you to have different version of an image for 4 differents screen sizes.">
+        <meta name="keywords" content="JavaScript, Responsive, image, images, jQuery, coffeescript, lazy loading">
+        <meta name="author" content="Julien Renaux">
+        <meta property="og:title" content="Demo Animations | BttrLazyLoading"/>
+        <meta property="og:description" content="BttrLazyLoading is a Jquery plugin that allows your web application to only load images within the viewport. It also allows you to have different version of an image for 4 differents screen sizes."/>
+        <meta property="og:url" content="http://bttrlazyloading.julienrenaux.fr/demo/animations.php"/>
+        <meta property="og:site_name" content="Julien Renaux Blog"/>
+        <meta property="og:type" content="blog"/>
+        <meta property="og:image" content="http://julienrenaux.fr/wp-content/uploads/2013/12/bttrlazyloading-300x181.png">
+        <link rel="stylesheet" type="text/css" href="../bower_components/bootswatch/lumen/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="../bower_components/font-awesome/css/font-awesome.min.css" />
+        <link rel="stylesheet" type="text/css" href="../BttrLazyLoading/dist/bttrlazyloading.min.css" />
+        <link rel="stylesheet" type="text/css" href="../bower_components/animate.css/animate.min.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script src="../bower_components/jquery/jquery.min.js"></script>
+        <script src="../bower_components/jquery.smooth-scroll/jquery.smooth-scroll.min.js"></script>
+        <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../bower_components/highlight.js/src/styles/solarized_dark.css" />
+        <script src="js/highlight.pack.js"></script>
+        <script src="../BttrLazyLoading/dist/jquery.bttrlazyloading.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Multiserve animations</h1>
+                <p>BttrLazyLoading allow you to have a specific animation per device.</p>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>HTML</h2>
+                    <pre><code class="html">&lt;img id=&quot;yourImg&quot; class=&quot;bttrlazyloading&quot;
+    data-bttrlazyloading-animation=&quot;flipInX&quot;
+    data-bttrlazyloading-xs-src=&quot;img/800x300.jpg&quot;
+    data-bttrlazyloading-xs-width=&quot;800&quot;
+    data-bttrlazyloading-xs-height=&quot;300&quot;
+    data-bttrlazyloading-sm-src=&quot;img/380x380.jpg&quot;
+    data-bttrlazyloading-sm-width=&quot;380&quot;
+    data-bttrlazyloading-sm-height=&quot;380&quot;
+    data-bttrlazyloading-md-src=&quot;img/350x350.jpg&quot;
+    data-bttrlazyloading-md-width=&quot;350&quot;
+    data-bttrlazyloading-md-height=&quot;350&quot;
+    data-bttrlazyloading-lg-src=&quot;img/300x300.jpg&quot;
+    data-bttrlazyloading-lg-width=&quot;300&quot;
+    data-bttrlazyloading-lg-height=&quot;300&quot;
+/&gt;</pre></code>
+                </div>
+                <div class="col-md-6">
+                    <h2>JavaScript</h2>
+                    <pre><code class="javascript">$('#yourImg').bttrlazyloading();</pre></code>
+                </div>
+            </div>
+            <div class="alert alert-info text-center"><b>OR</b></div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>HTML</h2>
+                    <pre><code class="html">&lt;img id=&quot;yourImg&quot; class=&quot;bttrlazyloading&quot; /&gt;</pre></code>
+                </div>
+                <div class="col-md-6">
+                    <h2>JavaScript</h2>
+                    <pre><code class="javascript">$('#yourImg').bttrlazyloading({
+	animation: 'flipInX' // Select among the CSS animations below
+	xs: {
+		src: 'http://placekitten.com/800/300',
+		width: 800,
+		height: 300
+	},
+	sm: {
+		src: 'http://placekitten.com/380/380',
+		width: 380,
+		height: 380
+	},
+	md: {
+		src: 'http://placekitten.com/350/350',
+		width: 350,
+		height: 350
+	},
+	lg: {
+		src: 'http://placekitten.com/300/300',
+		width: 300,
+		height: 300
+	}
+});</pre></code>
+                </div>
+            </div>
+
+            <div class="page-header">
+                <h2>Demo</h2>
+            </div>
+            <div class="row" id="loading-area"></div>
+            <div class="page-header">
+                <h2>Mode Demos</h2>
+            </div>
+            <?php include 'more-demo.php'; ?>
+        </div>
+        <?php include '../menu.php'; ?>
+        <script type="text/javascript">
+            $(function($, hljs) {
+                $('#back-to-top').click(function(event) {
+                    $.smoothScroll({
+                        scrollTarget: 'body',
+                        offset: -60
+                    });
+                });
+                hljs.initHighlightingOnLoad();
+
+                $('.bttrlazyloading').bttrlazyloading()
+                for (var i = 0; i < 31; i++)
+                {
+                    var $wrapper = $('<div class=" col-sm-6 col-md-4 col-lg-3">');
+                    var $img = $('<img class="bttrlazyloading">');
+                    $wrapper.append($img);
+                    $('#loading-area').append($wrapper);
+                    $img.bttrlazyloading({
+                        animation: 'bounceIn',
+                        delay: 1000,
+                        xs: {
+                            src: 'img/800x300.jpg',
+                            width: 800,
+                            height: 300,
+                            animation: 'bounceInDown'
+                        },
+                        sm: {
+                            src: 'img/380x380.jpg',
+                            width: 380,
+                            height: 380,
+                            animation: 'bounceInUp'
+                        },
+                        md: {
+                            src: 'img/350x350.jpg',
+                            width: 350,
+                            height: 350,
+                            animation: 'bounceInLeft'
+                        },
+                        lg: {
+                            src: 'img/300x300.jpg',
+                            width: 300,
+                            height: 300,
+                            animation: 'bounceInRight'
+                        }
+                    });
+                }
+
+            }(jQuery, hljs));
+        </script>
+        <script src="js/analytics.js"></script>
+    </body>
+</html>
